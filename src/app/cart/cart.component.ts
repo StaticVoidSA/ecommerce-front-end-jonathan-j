607a676433ac69c1e8c0c664659acb3a766ea6e8
JonathanReverside <jonathanj.jenkins@reverside.co.za>
2021-11-05 19:34:03 -0700

    2021-11-05

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -79,14 +79,14 @@ export class CartComponent implements OnInit {
             this.totalPrice = +this.cartHelper.calculateTotalPrice(this.items).toFixed(2);
             this.totalItems = this.cartHelper.getCartCount(this.items);
             this.isLoading = false;
-          }, 500);
+          }, 100);
         } else {
           setTimeout(() => {
             this.router.navigate(["/no-cart-items"]);
             this.isLoading = false;
-          }, 500);
+          }, 100);
         }
-      }, 2000);
+      }, 100);
     });
 
     async function Init() {
